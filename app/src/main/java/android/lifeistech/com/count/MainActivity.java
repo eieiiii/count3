a00da8be1d910584e91b58c43e0df77232784186
eieiiii <eikun.kawai@gmail.com>
2016-10-30 15:41:04 +0900

    first commit

---
+++ app/src/main/java/android/lifeistech/com/count/MainActivity.java
@@ -0,0 +1,25 @@
+package android.lifeistech.com.count;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int count = 0;
+    TextView countTextview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        countTextview = (TextView)findViewById(R.id.textView);
+        countTextview.setText(String.valueOf(count));
+    }
+
+    public void plus(View view){
+        count++;
+        countTextview.setText(String.valueOf(count));
+    }
+}
